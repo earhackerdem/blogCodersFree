eede63bb492dc5a5d83d08bea688fb9424f68d85
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-02-10 15:53:51 -0600

    10 - Crud de categoría

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\{CategoryController, HomeController};
 use Illuminate\Support\Facades\Route;
 
-Route::get('', [HomeController::class,'index']);
+Route::get('', [HomeController::class,'index'])->name('admin.home');
+
+Route::resource('categories', CategoryController::class)->names('admin.categories');
