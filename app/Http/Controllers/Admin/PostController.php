c08ba4d120f4170b5bae7a219117c75027942965
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-03-02 19:05:42 -0600

    18 - Policies en Laravel ¿Qué son y para que sirven?

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -82,6 +82,8 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+        $this->authorize('author',$post);
+
         $categories = Category::pluck('name','id');
         $tags = Tag::all();
         return view('admin.posts.edit',compact('post','categories','tags'));
@@ -96,6 +98,8 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, Post $post)
     {
+        $this->authorize('author',$post);
+
         $post->update($request->all());
 
         if($request->file('file')){
@@ -131,6 +135,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        $this->authorize('author',$post);
+
         $post->delete();
         return redirect()->route('admin.posts.index')->with('info','El post se elimino con éxito');
     }
